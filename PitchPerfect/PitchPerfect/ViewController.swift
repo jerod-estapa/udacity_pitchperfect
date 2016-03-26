75bee57902945ed063b6912fb582c40c81f0f078
jerodestapa <jerodestapa@gmail.com>
2016-03-26 11:31:56 -0500

    Repaired UI links

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by User on 3/26/16.
+//  Copyright © 2016 Jerod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(sender: AnyObject) {
+        print("Record button pressed!")
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# udacity_pitchperfect
+My repo for Udacity's iOS Intro to iOS App Development course
+
+https://www.udacity.com/course/intro-to-ios-app-development-with-swift--ud585
