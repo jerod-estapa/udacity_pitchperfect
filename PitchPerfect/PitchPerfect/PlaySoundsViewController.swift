60b8421681cbb366ae71eaf626c6bbb5c309899b
jerodestapa <jerodestapa@gmail.com>
2016-03-28 00:41:08 -0500

    Added switch statements for buttons

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -2,19 +2,69 @@
 //  PlaySoundsViewController.swift
 //  PitchPerfect
 //
-//  Created by User on 3/27/16.
-//  Copyright © 2016 Jerod. All rights reserved.
+//  Created by Jerod on 3/27/16.
+//  Copyright © 2016 Jerod Estapa. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int { case Slow = 0,  Fast, Chipmunk, Vader, Echo, Reverb }
+    
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("Play Sound Button pressed!")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(rate: 1000)
+        case .Vader:
+            playSound(rate: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        
+        configureUI(.Playing)
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("Stop Audio Button pressed!")
+    }
+    
+    var recordedAudio : NSURL!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("PlaySoundsViewController loaded!")
+        setupAudio()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
